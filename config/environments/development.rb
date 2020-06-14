e80b5bf6a462cc849750b1835bbea8c7ec0e13d6
makikashiwai <mmmkpn@gmail.com>
2020-06-14 21:41:07 +0900

    本番環境新規登録エラー修正2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'example.com'}
+  config.action_mailer.default_url_options = { host: Rails.application.credentials[:home_page][:ip_address]}
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   #デプロイする時消した所
   # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -65,6 +66,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "first-app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: Rails.application.credentials[:home_page][:ip_address]}
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'first-app@gmail.com',
+    password: 'kimhyonuMK0720',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
