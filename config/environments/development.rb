e813e0b0cabf195456fa3637f99bfb8ae638979d
makikashiwai <mmmkpn@gmail.com>
2020-06-14 21:22:21 +0900

    本番環境　新規登録エラー修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'example.com'}
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
